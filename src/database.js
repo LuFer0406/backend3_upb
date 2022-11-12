82a9d3ff1758332ffe2255b611244fad48f8d3a2
LuisaFernanda <luisamoralesocp@gmail.com>
2022-11-12 09:32:10 -0500

    Backend3 cambio msg database.js

--- src/database.js
+++ src/database.js
@@ -7,7 +7,7 @@ const uri = "mongodb+srv://root:1234@cluster0.iifcrfm.mongodb.net/dbpost";
 export const conectarDb = async () => {
   try {
     const db = await mongoose.connect(uri);
-    console.log("Base de datos conectada con éxito", db.connection.name);
+    console.log("Base de datos conectada correctamente.", db.connection.name);
   } catch (error) {
     console.log(
       `Ha sucedido un error al conectar con la base de datos ${error.message}`
